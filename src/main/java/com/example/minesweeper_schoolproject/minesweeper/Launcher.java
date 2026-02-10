a4c1853a38618047da5bb0f1ec8bbd0dc0097f6c
bivorzk <bzkugli@gmail.com>
2026-02-10 14:00:29 +0100

    initial commit

---
+++ src/main/java/com/example/minesweeper_schoolproject/minesweeper/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.minesweeper_schoolproject.minesweeper;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
